4aa39670e61a7443ac4541bd7ff37bbbbf3b3227
Timothé Viot <timothe.viot@insa-rennes.fr>
2014-10-07 10:32:04 +0200

    first commit

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,17 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    /**
+     * Main method lol
+     * @param args the args
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
